053c957d927b7bf1fdabae86f3e6d6941c8c7ffd
David J. Felix <felix.davidj@gmail.com>
2022-01-22 10:26:20 -0600

    feat: create a core brewfile

---
+++ Brewfile.core.rb
@@ -0,0 +1,23 @@
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/core"
+brew "mas"
+cask "1password"
+cask "discord"
+cask "google-chrome"
+cask "kap"
+cask "little-snitch"
+cask "micro-snitch"
+cask "rectangle"
+cask "spotify"
+cask "visual-studio-code"
+cask "vlc"
+cask "whatsapp"
+cask "zoom"
+mas "Amphetamine", id: 937984704
+mas "Gifski", id: 1351639930
+mas "Keynote", id: 409183694
+mas "Numbers", id: 409203825
+mas "OneDrive", id: 823766827
+mas "Pages", id: 409201541
+mas "Slack", id: 803453959
