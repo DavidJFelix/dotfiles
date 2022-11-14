c227b78122b3219ff378ff0a9269e3b45e8c6781
David J. Felix <felix.davidj@gmail.com>
2022-11-14 15:32:54 -0600

    fix: add svn to font brewfile

--- Brewfile.fonts.rb
+++ Brewfile.fonts.rb
@@ -3,6 +3,8 @@ tap "homebrew/cask"
 tap "homebrew/cask-fonts"
 tap "homebrew/core"
 
+# SVN is needed for many fonts
+brew "svn"
 cask "font-arvo"
 cask "font-fira-mono-for-powerline"
 cask "font-jetbrains-mono"
